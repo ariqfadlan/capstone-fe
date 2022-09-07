a0f60ef12322115eda65de4e4c824ae37b4bd721
Ariq Fadlan <ariqfadlan@hotmail.co.id>
2022-09-07 14:52:58 +0700

    chore: remove default components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import VueApexCharts from "vue3-apexcharts";
 import DashboardLayout from "./components/DashboardLayout.vue";
 import EmptyLayout from "./components/EmptyLayout.vue";
@@ -6,8 +7,11 @@ import "./assets/tailwind.css";
 
 import App from "./App.vue";
 import router from "./router";
+
+const pinia = createPinia()
 const app = createApp(App);
+
 app.component("default-layout", DashboardLayout);
 app.component("empty-layout", EmptyLayout);
 
-app.use(router).use(VueApexCharts).mount("#app");
+app.use(router).use(pinia).use(VueApexCharts).mount("#app");
