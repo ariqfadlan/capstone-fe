53b6b01064e7d95af3a400358abf5763ef178245
Ariq Fadlan <ariqfadlan@hotmail.co.id>
2022-09-12 20:56:42 +0700

    fix(user): prompt title

--- src/components/users/UserDelete.vue
+++ src/components/users/UserDelete.vue
@@ -12,25 +12,12 @@
       class="z-50 w-11/12 mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-md"
     >
       <!-- Add margin if you want to see some of the overlay behind the modal-->
-      <div class="px-6 py-4 text-left modal-content">
-        <svg
-          class="mx-auto mb-4 w-14 h-14 text-red-400"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-          />
-        </svg>
+      <div class="mt-2 px-6 py-4 text-left modal-content">
+        <ExclamationCircleIcon class="mx-auto mb-4 w-14 h-14 text-red-400" />
 
         <!--Title-->
         <div class="flex items-center justify-center pb-3">
-          <p class="text-2xl font-bold">Terjadi Galat!</p>
+          <p class="text-2xl font-bold">Perhatian!</p>
           <div
             class="z-50 cursor-pointer modal-close"
             @click="$emit('toggleDeleteModal')"
@@ -63,6 +50,7 @@
 import { computed } from "vue";
 import { useUserStore } from "@/store/users";
 import { useToast } from "vue-toastification";
+import { ExclamationCircleIcon } from "@heroicons/vue/24/outline";
 
 const toast = useToast();
 const emits = defineEmits(["toggleDeleteModal"]);
