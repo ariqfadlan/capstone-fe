45e578c948d6a190f94fcf35ef3c66a937f76ec9
Ariq Fadlan <ariqfadlan@hotmail.co.id>
2022-09-21 20:50:26 +0700

    feat(collections): add display image

--- src/components/collections/CollectionRead.vue
+++ src/components/collections/CollectionRead.vue
@@ -17,7 +17,12 @@
     <div class="mt-4">
       <div class="p-6 bg-white rounded-md shadow-md">
         <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-          <div class="mb-2 pb-2 border-b border-gray40">
+          <div class="mb-2 pb-2 sm:row-span-4 sm:-order-1">
+            <div class="text-gray-600">Gambar</div>
+            <img :src="`http://${result.imagePath}`" class="mt-2 max-h-72" />
+          </div>
+
+          <div class="mb-2 pb-2 border-b border-gray40 sm:-order-2">
             <div class="text-gray-600">Nama</div>
             <div class="text-gray-700 font-semibold text-lg" for="username">
               {{ result.name }}
