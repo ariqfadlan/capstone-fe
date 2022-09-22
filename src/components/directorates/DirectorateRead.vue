19593d92629d82e72985a7f0be98362a60ac40e9
Ariq Fadlan <ariqfadlan@hotmail.co.id>
2022-09-22 16:19:04 +0700

    fix(directorate): button link update

--- src/components/directorates/DirectorateRead.vue
+++ src/components/directorates/DirectorateRead.vue
@@ -8,7 +8,7 @@
         Close
       </button>
       <button
-        @click="$router.push({ name: 'UserUpdate', params: { id } })"
+        @click="$router.push({ name: 'DirectorateEdit', params: { id } })"
         class="ml-2 px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none"
       >
         Edit
