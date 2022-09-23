6b28adf103bcab1d3455c0ae8d57cc2dab243fef
Ariq Fadlan <ariqfadlan@hotmail.co.id>
2022-09-23 22:13:46 +0700

    fix(employees): update type

--- src/types/employees.ts
+++ src/types/employees.ts
@@ -3,6 +3,7 @@ import type { IUserData } from "./users";
 export interface IEmployeeData {
   id: number;
   fullname: string;
+  employeeNumber: string;
   user: IUserData;
   directorate: {
     id: number;
